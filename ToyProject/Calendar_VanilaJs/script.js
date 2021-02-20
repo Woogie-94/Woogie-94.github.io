d419a9c0124edcc4545d2b5f4288d565775ce8b5
Woogie-94 <keu1106@naver.com>
2021-02-20 22:16:33 +0900

    createDayComponent function 수정

--- ToyProject/Calendar_VanilaJs/script.js
+++ ToyProject/Calendar_VanilaJs/script.js
@@ -1,14 +1,17 @@
 const calendarCon = document.querySelector(".calendar");
-const nextBtn = document.querySelector(".next_btn");
-const prevBtn = document.querySelector(".prev_btn");
+const nextBtn = document.querySelector(".calendar_btn__next");
+const prevBtn = document.querySelector(".calendar_btn__prev");
 
 window.onload = () => {
   paintingCalendar();
 };
 
 const paintingCalendar = () => {
+  const yy = document.querySelector(".calendar_month__yy");
+  const mm = document.querySelector(".calendar_month__mm");
   const calendarData = createCalendarData();
-  calendarCon.textContent = today.format("YYYY-MM");
+  yy.textContent = `${today.format("YYYY")}`;
+  mm.textContent = `${today.format("MM")}월`;
 
   calendarData.map((week, weekIdx) => {
     createWeekComponent();
@@ -20,38 +23,44 @@ const paintingCalendar = () => {
 
 const createWeekComponent = () => {
   const weekElement = document.createElement("div");
-  weekElement.classList.add("week_component");
+  weekElement.classList.add("calendar__week", "week_data_components");
 
+  const blankDayElement = document.createElement("div");
+  blankDayElement.classList.add("calendar__day");
+
+  weekElement.append(blankDayElement);
   calendarCon.append(weekElement);
 };
 
 const createDayComponent = (day, weekIdx) => {
-  const weekContainer = document.querySelectorAll(".week_component");
-  const dayElement = document.createElement("span");
-  dayElement.classList.add("day_component");
-  dayElement.textContent = day.format("DD") + " / ";
+  const weekContainer = document.querySelectorAll(".week_data_components");
+  const dayElement = document.createElement("div");
+  dayElement.classList.add("calendar__day", "day_data_components");
+  dayElement.textContent = day.format("D");
+
+  const sameMonth = today.format("MM") === day.format("MM");
+  if (sameMonth) dayElement.classList.add("day_data_components--on");
+  else dayElement.classList.add("day_data_components--off");
 
   weekContainer[weekIdx].append(dayElement);
 };
 
-nextBtn.addEventListener("click", () => {
-  const dayElement = document.querySelectorAll(".week_component");
+const removeCalendar = () => {
+  const dayElement = document.querySelectorAll(".week_data_components");
 
   for (let i = 0; i < dayElement.length; i++) {
     calendarCon.removeChild(dayElement[i]);
   }
+};
 
+nextBtn.addEventListener("click", () => {
+  removeCalendar();
   nextMonth();
   paintingCalendar();
 });
 
 prevBtn.addEventListener("click", () => {
-  const dayElement = document.querySelectorAll(".week_component");
-
-  for (let i = 0; i < dayElement.length; i++) {
-    calendarCon.removeChild(dayElement[i]);
-  }
-
+  removeCalendar();
   prevMonth();
   paintingCalendar();
 });
