70dae8ab3c95f2aab82fda8d224623135d7983e1
Woogie <59603529+Woogie-94@users.noreply.github.com>
2021-08-27 05:06:11 +0900

    Delete room directory

--- room/js/utill.js
+++
@@ -1,8 +0,0 @@
-const debounce = (func, delay) => {
-  let timeoutId = null;
-
-  return (...args) => {
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(func.bind(null, ...args), delay);
-  };
-};
